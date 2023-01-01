d7fa048ac6c566ac6c76ddbee2e9b72c411801b0
Tiz-Dankan <tibesigwadankan@gmail.com>
2023-01-01 15:47:32 +0300

    feat: show hide edit profile form

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,9 +1,11 @@
-import React, { Fragment, useRef, useState } from "react";
+import React, { Fragment, useState } from "react";
 import Loading from "../../components/UI/Loading/Loading";
 import { IconContext } from "react-icons";
 import { IoPersonCircleSharp } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
 import { updateProfile } from "../../store/actions/auth";
+// import { authenticate } from "../../store/actions/auth";
 import styles from "./Profile.module.scss";
 
 const Profile = () => {
@@ -14,6 +16,15 @@ const Profile = () => {
   const [userName, setUserName] = useState(user.userName);
   const token = useSelector((state) => state.auth.token);
   const dispatch = useDispatch();
+
+  // if (performance.getEntriesByType("navigation")[0].type === "reload") {
+  //   const userData = localStorage.getItem("userData");
+  //   const parsedData = JSON.parse(userData);
+  //   console.log("parsedData");
+  //   console.log(parsedData);
+
+  //   dispatch(authenticate(parsedData.user, parsedData.token));
+  // }
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -41,7 +52,6 @@ const Profile = () => {
     if (!userId || !userName || !email) return;
     try {
       setIsLoading(true);
-      // dispatch an action
       await dispatch(updateProfile(userId, userName, email, token));
       setIsLoading(false);
     } catch (error) {
@@ -55,87 +65,139 @@ const Profile = () => {
         <div className={styles["profile__title"]}>
           <span className={styles["profile__title--text"]}>Profile</span>
         </div>
-        <div className={styles["profile__user-data"]}>
-          <div className={styles["profile__user-data__image"]}>
-            {user.imageUrl && (
-              <div className={styles["profile__user-data__image--image"]}>
-                {/* Actual user image */}
-              </div>
-            )}
-            {!user.imageUrl && (
-              <span
-                className={styles["profile__user-data__image--placeholder"]}
-              >
-                <IconContext.Provider
-                  value={{
-                    size: "4rem",
-                  }}
-                >
-                  <IoPersonCircleSharp />
-                </IconContext.Provider>
-              </span>
-            )}
-
-            <div className={styles["profile__user-data__image--actions"]}>
+        {!showEditForm && (
+          <div className={styles["profile__user-data"]}>
+            <div className={styles["profile__user-data__image"]}>
               {user.imageUrl && (
-                <button
-                  className={
-                    styles["profile__user-data__image--actions--upload-btn"]
-                  }
-                >
-                  Update Image
-                </button>
+                <div className={styles["profile__user-data__image--image"]}>
+                  {/* Actual user image */}
+                </div>
               )}
               {!user.imageUrl && (
-                <button
+                <span
+                  className={styles["profile__user-data__image--placeholder"]}
+                >
+                  <IconContext.Provider
+                    value={{
+                      className: styles["person-icon"],
+                    }}
+                  >
+                    <IoPersonCircleSharp />
+                  </IconContext.Provider>
+                </span>
+              )}
+              <div className={styles["profile__user-data__image--actions"]}>
+                {user.imageUrl && (
+                  <button
+                    className={
+                      styles["profile__user-data__image--actions--upload-btn"]
+                    }
+                  >
+                    Update Image
+                  </button>
+                )}
+                {!user.imageUrl && (
+                  <button
+                    className={
+                      styles["profile__user-data__image--actions--upload-btn"]
+                    }
+                  >
+                    Add Image
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className={styles["profile__user-data__details"]}>
+              <div className={styles["profile__user-data__details__name"]}>
+                <span
+                  className={styles["profile__user-data__details__name--title"]}
+                >
+                  Username
+                </span>
+                <span
+                  className={styles["profile__user-data__details__name--value"]}
+                >
+                  {userName}
+                </span>
+              </div>
+              <div className={styles["profile__user-data__details__email"]}>
+                <span
                   className={
-                    styles["profile__user-data__image--actions--upload-btn"]
+                    styles["profile__user-data__details__email--title"]
                   }
                 >
-                  Add Image
-                </button>
-              )}
+                  Email
+                </span>
+                <span
+                  className={
+                    styles["profile__user-data__details__email--value"]
+                  }
+                >
+                  {email}
+                </span>
+              </div>
+              <div className={styles["profile__user-data__details__btn"]}>
+                <button onClick={() => showHideEditForm()}>Edit Profile</button>
+              </div>
             </div>
           </div>
-        </div>
-        <form
-          className={styles["profile__form"]}
-          onSubmit={(event) => updateProfileHandler(event)}
-        >
-          <h4 className={styles["profile__form__heading"]}>
-            Edit your Profile
-          </h4>
-          <div className={styles["profile__form__input"]}>
-            <label>User name*</label>
-            <input
-              type="text"
-              placeholder="User name"
-              className={styles["profile__form__input--field"]}
-              value={userName}
-              onChange={(event) => handleUserNameChange(event)}
-              required
-            />
-          </div>
-          <div className={styles["profile__form__input"]}>
-            <label>Email address*</label>
-            <input
-              type="email"
-              placeholder="Email"
-              className={styles["profile__form__input--field"]}
-              value={email}
-              onChange={(event) => handleEmailChange(event)}
-              required
-            />
-          </div>
-          <div className={styles["button__container"]}>
-            {!isLoading && (
-              <button type="submit" id="button" className={styles["form-btn"]}>
-                Update
-              </button>
-            )}
-            {isLoading && <Loading event={"on-form-submit"} />}
-          </div>
-        </form>
+        )}
+        {showEditForm && (
+          <form
+            className={styles["profile__form"]}
+            onSubmit={(event) => updateProfileHandler(event)}
+          >
+            <h4 className={styles["profile__form__heading"]}>
+              Edit your Profile
+            </h4>
+            <span
+              onClick={() => showHideEditForm()}
+              className={styles["profile__form__hide-action"]}
+            >
+              <IconContext.Provider
+                value={{
+                  size: "2rem",
+                }}
+              >
+                <IoMdClose />
+              </IconContext.Provider>
+            </span>
+            <div className={styles["profile__form__input"]}>
+              <label>User name*</label>
+              <input
+                type="text"
+                placeholder="User name"
+                className={styles["profile__form__input--field"]}
+                value={userName}
+                onChange={(event) => handleUserNameChange(event)}
+                required
+              />
+            </div>
+            <div className={styles["profile__form__input"]}>
+              <label>Email address*</label>
+              <input
+                type="email"
+                placeholder="Email"
+                className={styles["profile__form__input--field"]}
+                value={email}
+                onChange={(event) => handleEmailChange(event)}
+                required
+              />
+            </div>
+            <div className={styles["button__container"]}>
+              {!isLoading && (
+                <button
+                  type="submit"
+                  id="button"
+                  className={styles["form-btn"]}
+                >
+                  Update
+                </button>
+              )}
+              {isLoading && <Loading event={"on-form-submit"} />}
+            </div>
+          </form>
+        )}
         <footer className={styles["profile__footer"]}>
           <span>
             LetsChat &copy; {new Date().getFullYear()}. All rights reserved

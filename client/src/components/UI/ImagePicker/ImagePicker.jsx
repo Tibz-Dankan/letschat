f867f1af585673964978c5b8f50239e5f6e511b9
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-01-03 10:04:05 +0300

    feat(ImagePicker): enable users to select images from computer

--- client/src/components/UI/ImagePicker/ImagePicker.jsx
+++ client/src/components/UI/ImagePicker/ImagePicker.jsx
@@ -1,7 +1,104 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
+import { useFilePicker } from "use-file-picker";
+import { BsFillCloudArrowUpFill } from "react-icons/bs";
+import { IoCheckmarkDoneSharp } from "react-icons/io5";
+import { FaWindowClose } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import styles from "./ImagePicker.module.scss";
 
-const ImagePicker = () => {
-  return <div>ImagePicker</div>;
+const ImagePicker = (props) => {
+  const [photo, setPhoto] = useState("");
+
+  const [openFileSelector, { filesContent }] = useFilePicker({
+    readAs: "DataURL",
+    accept: "image/*",
+    multiple: false,
+    limitFilesConfig: { max: 1 },
+
+    maxFileSize: 50,
+  });
+  useEffect(() => {
+    filesContent.map((file) => {
+      return setPhoto(file.content);
+    });
+  }, [filesContent]);
+
+  const saveHandler = () => {
+    props.onSave(photo);
+  };
+
+  return (
+    <Fragment>
+      <div className={styles["image-picker"]}>
+        {!photo && (
+          <button
+            onClick={() => openFileSelector()}
+            className={styles["image-picker__btn"]}
+          >
+            <span className={styles["image-picker__btn--icon"]}>
+              <IconContext.Provider value={{ size: "2rem" }}>
+                <BsFillCloudArrowUpFill />
+              </IconContext.Provider>
+            </span>
+            <span className={styles["image-picker--label"]}>Select Photo</span>
+          </button>
+        )}
+        {photo && (
+          <div className={styles["image-picker__preview"]}>
+            <img
+              className={styles["image-picker__preview--photo"]}
+              src={photo}
+              alt="user-photo"
+            />
+            <div className={styles["image-picker__preview__actions"]}>
+              <button
+                onClick={() => setPhoto("")}
+                className={styles["image-picker__preview__actions__btn"]}
+              >
+                <span
+                  className={
+                    styles["image-picker__preview__actions__btn--icon"]
+                  }
+                >
+                  <IconContext.Provider value={{ size: "2rem" }}>
+                    <FaWindowClose />
+                  </IconContext.Provider>
+                </span>
+                <span
+                  className={
+                    styles["image-picker__preview__actions__btn--label"]
+                  }
+                >
+                  Cancel
+                </span>
+              </button>
+              <button
+                onClick={() => saveHandler()}
+                className={styles["image-picker__preview__actions__btn"]}
+              >
+                <span
+                  className={
+                    styles["image-picker__preview__actions__btn--icon"]
+                  }
+                >
+                  <IconContext.Provider value={{ size: "2rem" }}>
+                    <IoCheckmarkDoneSharp />
+                  </IconContext.Provider>
+                </span>
+                <span
+                  className={
+                    styles["image-picker__preview__actions__btn--label"]
+                  }
+                >
+                  Done
+                </span>
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </Fragment>
+  );
 };
 
 export default ImagePicker;

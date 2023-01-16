b319fea48bf83fce5bfc2d58de57c2c967f10518
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-01-16 14:09:14 +0300

    feat: chat UI styles

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 import styles from "./Chat.module.scss";
 import ChatMates from "../../components/UI/ChatMates/ChatMates";
+import bgImageFour from "../../assets/bg-image-four.jpeg";
 
 const Chat = ({ socket }) => {
   return (
     <Fragment>
-      <div className={styles["chat-page"]}>
+      <div className={styles["chat"]}>
         <ChatMates socket={socket} />
+        <div className={styles["chat__welcome-bg"]}>
+          <h3>LetsChat</h3>
+        </div>
       </div>
     </Fragment>
   );

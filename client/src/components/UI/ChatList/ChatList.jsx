04907857c4c4d2f098caff8c8d36ef0a3bda9e38
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-01-16 12:13:31 +0300

    feat: ChatList UI styles improved

--- client/src/components/UI/ChatList/ChatList.jsx
+++ client/src/components/UI/ChatList/ChatList.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./ChatList.module.scss";
+import styledScrollbar from "../../../styles/scrollbar.module.scss";
 import { IconContext } from "react-icons";
 import { IoPersonCircleSharp } from "react-icons/io5";
 import { useSelector, useDispatch } from "react-redux";
 import { updateChatMateData } from "../../../store/actions/chat";
 import { generateChatRoomId } from "../../../utils/generateChatRoomId";
+import Image from "../Image/Image";
 
 const ChatList = ({ socket }) => {
   const chatMates = useSelector((state) => state.chat.allChatMates);
@@ -28,54 +30,61 @@ const ChatList = ({ socket }) => {
           <h5>CHATS</h5>
           <h5>EXPLORE</h5>
         </div>
-        {chatMates.map((chatMate) => {
-          return (
-            <div
-              key={chatMate.userId}
-              className={styles["chat__list__chat-mate"]}
-            >
-              {!chatMate.imageUrl && (
-                <span
-                  className={styles["chat__list__chat-mate--image-placeholder"]}
-                >
-                  <IconContext.Provider value={{ size: "4rem" }}>
-                    <IoPersonCircleSharp />
-                  </IconContext.Provider>
-                </span>
-              )}
-              {chatMate.imageUrl && (
-                <div className={styles["chat__list__chat-mate--image"]}>
-                  <img src={chatMate.imageUrl} alt="Profile pic" />
-                </div>
-              )}
+        <div
+          className={`${styles["chat__list__content"]} ${styledScrollbar["scroll-bar"]}`}
+        >
+          {chatMates.map((chatMate) => {
+            return (
               <div
-                onClick={() => joinRoom(chatMate)}
-                className={styles["chat__list__chat-mate__data"]}
+                key={chatMate.userId}
+                className={styles["chat__list__chat-mate"]}
               >
-                <span className={styles["chat__list__chat-mate__data--name"]}>
-                  {chatMate.userName}
-                </span>
-                <span
-                  className={
-                    styles["chat__list__chat-mate__data--last-message"]
-                  }
+                {!chatMate.imageUrl && (
+                  <span
+                    className={
+                      styles["chat__list__chat-mate--image-placeholder"]
+                    }
+                  >
+                    <IconContext.Provider value={{ size: "4.8rem" }}>
+                      <IoPersonCircleSharp />
+                    </IconContext.Provider>
+                  </span>
+                )}
+                {chatMate.imageUrl && (
+                  <div className={styles["chat__list__chat-mate--image"]}>
+                    <Image
+                      src={chatMate.imageUrl}
+                      alt={"photo"}
+                      class={styles["image"]}
+                    />
+                  </div>
+                )}
+                <div
+                  onClick={() => joinRoom(chatMate)}
+                  className={styles["chat__list__chat-mate__data"]}
                 >
-                  last message text
-                </span>
-                <span
-                  className={
-                    styles["chat__list__chat-mate__data--message-date"]
-                  }
-                >
-                  Date Or Time
-                </span>
+                  <span className={styles["chat__list__chat-mate__data--name"]}>
+                    {chatMate.userName}
+                  </span>
+                  <span
+                    className={
+                      styles["chat__list__chat-mate__data--last-message"]
+                    }
+                  >
+                    last message text
+                  </span>
+                  <span
+                    className={
+                      styles["chat__list__chat-mate__data--message-date"]
+                    }
+                  >
+                    Date Or Time
+                  </span>
+                </div>
               </div>
-            </div>
-          );
-        })}
-        <footer className={styles["chat__list__footer"]}>
-          <span>LetsChat &copy; {new Date().getFullYear()}</span>
-        </footer>
+            );
+          })}
+        </div>
       </div>
     </Fragment>
   );

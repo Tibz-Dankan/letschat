5b49362fae8054558a746bcc03ec0594db36949d
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-01-19 23:40:53 +0300

    feat: Home page content added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,21 +1,43 @@
-/* eslint-disable no-unused-vars */
-import React, { Fragment } from "react";
-import styles from "./Home.module.scss";
-import Footer from "../../components/layouts/Footer/Footer";
-import LogOut from "../../components/UI/LogOut/LogOut";
-import { useSelector } from "react-redux";
+import React from "react";
+import "./landing-page.scss";
 
 const Home = () => {
-  // const token = useSelector((state) => state.auth.token);
-  // const user = useSelector((state) => state.auth.user);
-  // console.log("token: " + token + " user: " + JSON.stringify({ user }));
+  const currentYear = new Date().getFullYear();
+
   return (
-    <Fragment>
-      <div className={styles["home__page"]}>
-        <h1>LetsChat Home page</h1>
-        <Footer />
-      </div>
-    </Fragment>
+    <div
+      className="landing-page"
+      style={{ backgroundColor: "hsl(210, 14%, 83.1%)" }}
+    >
+      <header className="landing-page__header" style={{ height: "100vh" }}>
+        <div className="landing-page__header-logo">LetsChat</div>
+        <nav className="landing-page__header-nav">
+          <ul>
+            <li>About</li>
+            <li>Login</li>
+            <li>Signup</li>
+          </ul>
+        </nav>
+        <div className="landing-page__header-content">
+          <div className="landing-page__header-text">
+            <p>Get started</p>
+            <button style={{ backgroundColor: "hsl(187.4, 80.2%, 41.6%)" }}>
+              Click here
+            </button>
+          </div>
+          <div className="landing-page__header-image"></div>
+        </div>
+      </header>
+      <section className="landing-page__about">
+        <div className="landing-page__about-text">
+          <p>About text goes here</p>
+        </div>
+        <div className="landing-page__about-image"></div>
+      </section>
+      <footer className="landing-page__footer">
+        <p>LetsChat &copy; {currentYear} Tibz. All rights reserved.</p>
+      </footer>
+    </div>
   );
 };
 

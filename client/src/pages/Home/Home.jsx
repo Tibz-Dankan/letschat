f39afa41a7d9a88a8be7d84029527e294660ecc0
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-01-20 00:11:19 +0300

    fix: scss file import file path corrected

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,43 +1,38 @@
-import React from "react";
-import "./landing-page.scss";
+import React, { Fragment } from "react";
+import styles from "./Home.module.scss";
 
 const Home = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
-    <div
-      className="landing-page"
-      style={{ backgroundColor: "hsl(210, 14%, 83.1%)" }}
-    >
-      <header className="landing-page__header" style={{ height: "100vh" }}>
-        <div className="landing-page__header-logo">LetsChat</div>
-        <nav className="landing-page__header-nav">
-          <ul>
-            <li>About</li>
-            <li>Login</li>
-            <li>Signup</li>
-          </ul>
-        </nav>
-        <div className="landing-page__header-content">
-          <div className="landing-page__header-text">
-            <p>Get started</p>
-            <button style={{ backgroundColor: "hsl(187.4, 80.2%, 41.6%)" }}>
-              Click here
-            </button>
+    <Fragment>
+      <div className={styles["home"]}>
+        <header className={styles["home__header"]}>
+          <div className={styles["home__header-logo"]}>LetsChat</div>
+          <nav className={styles["home__header-nav"]}>
+            <ul>
+              <li>About</li>
+              <li>Login</li>
+              <li>Signup</li>
+            </ul>
+          </nav>
+          <div className={styles["home__header-content"]}>
+            <div className={styles["home__header-text"]}>
+              <p>Get started</p>
+              <button>Click here</button>
+            </div>
+            <div className={styles["home__header-image"]}></div>
           </div>
-          <div className="landing-page__header-image"></div>
-        </div>
-      </header>
-      <section className="landing-page__about">
-        <div className="landing-page__about-text">
-          <p>About text goes here</p>
-        </div>
-        <div className="landing-page__about-image"></div>
-      </section>
-      <footer className="landing-page__footer">
-        <p>LetsChat &copy; {currentYear} Tibz. All rights reserved.</p>
-      </footer>
-    </div>
+        </header>
+        <section className={styles["home__about"]}>
+          <div className={styles["home__about-text"]}>
+            <p>About text goes here</p>
+          </div>
+          <div className={styles["home__about-image"]}></div>
+        </section>
+        <footer className={styles["home__footer"]}>
+          <p>LetsChat &copy; {new Date().getFullYear()}. All rights reserved</p>
+        </footer>
+      </div>
+    </Fragment>
   );
 };
 

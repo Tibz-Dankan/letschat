4003c413596aac186b6512e3d5f084579b6b57cb
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-01-21 04:21:58 +0300

    feat: about content added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import { Link, NavLink } from "react-router-dom";
-import chatImage from "../../assets/chat-image-one.png";
 import styles from "./Home.module.scss";
 
 const Home = () => {
@@ -43,8 +42,13 @@ const Home = () => {
                 Chat with anyone
               </p>
               <p className={styles["home__header__content-text--detail"]}>
-                LetsChat is online messaging platform that lets you chat with
-                pretty much anyone available on the platform.
+                LetsChat is an online messaging platform that allows you to
+                connect and communicate with a diverse group of users who are
+                available on the platform at any given time. It offers a
+                convenient and easy way to engage in conversations with people
+                from all over the world. With its user-friendly interface, it
+                makes it easy for anyone to join and start chatting in a matter
+                of minutes.
               </p>
               <button>
                 <Link to="signup" className={styles["link"]}>
@@ -56,13 +60,21 @@ const Home = () => {
           </div>
         </header>
         <section id="about" className={styles["home__about"]}>
+          <div className={styles["home__about-image"]}></div>
           <div className={styles["home__about-text"]}>
+            <p className={styles["home__about-text--title"]}>About</p>
             <p>
-              LetsChat is online messaging platform that lets you chat with
-              pretty much anyone on the platform
+              LetsChat is a messaging platform that allows users to communicate
+              with anyone in real-time. It is designed for both personal and
+              professional use, with a focus on simplicity and speed. The
+              platform is easy to use, with an intuitive interface that makes it
+              easy to navigate and find the features you need. Whether you're
+              keeping in touch with friends and family or collaborating with
+              colleagues, LetsChat is a reliable and user-friendly messaging
+              platform that makes it easy to stay in touch with the people that
+              matter most.
             </p>
           </div>
-          <div className={styles["home__about-image"]}></div>
         </section>
         <footer className={styles["home__footer"]}>
           <span>
